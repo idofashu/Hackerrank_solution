7d7a4422eb8a45c41087cb276a432843fc0d1352
Ashutosh Kumar <60702324+idofashu@users.noreply.github.com>
2020-05-13 14:56:09 +0530

    Update solution.java

--- Java/Strings/Java Regex/solution.java
+++ Java/Strings/Java Regex/solution.java
@@ -14,6 +14,6 @@ class Solution{
     }
 }
 class MyRegex{
-    String zeroTo255="(\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|20[0-5])";
-    String pattern=zeroTo255 + "\\." + zeroTo255+ "\\." + zeroTo255+ "\\." + zeroTo255;
+    String zeroTo255 ="(\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|25[0-5])";
+    public String pattern = zeroTo255 + "\\." + zeroTo255 + "\\." + zeroTo255 + "\\." + zeroTo255;
 }
